106e838649248f76abfcb84d037df523344e4226
YS Chung <hello@yschung.org>
2023-01-22 18:03:13 +0800

    Use static instead of serverless build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import svelte from "@astrojs/svelte"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/static"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
   output: "static",
   adapter: vercel()
-});
+})
