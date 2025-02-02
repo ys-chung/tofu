6952587f9f7d17d9041ed1f0f16795e9f0616260
YS Chung <hello@yschung.org>
2025-02-02 21:40:27 +0800

    February 2025 update

--- src/components/solid/cells/ControlCell.tsx
+++ src/components/solid/cells/ControlCell.tsx
@@ -67,12 +67,12 @@ export const ControlCell = (props: {
     <div class="col-span-2 row-span-2 grid aspect-square grid-cols-2 gap-[1px] sm:col-span-1 sm:row-span-1">
       {/* Input */}
       <div class="flex aspect-square flex-col gap-2 bg-white p-2 justify-between">
-        <h2 class="text-sm font-semibold">Character</h2>
+        <h2 class="text-sm font-semibold">Type here</h2>
         <div>
           <input
             type="text"
             name="character"
-            class="block w-full text-7xl outline-none sm:text-5xl"
+            class="block w-full text-7xl outline-none sm:text-5xl caret-neutral-500"
             oninput={onInput}
             oncompositionend={onCompositionEnd}
             aria-label="Character"
