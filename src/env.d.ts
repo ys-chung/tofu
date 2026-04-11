b42a959fe6cf3d4a7649b076908b1ead91e48c9c
YS Chung <hello@yschung.org>
2026-04-11 10:57:16 +0800

    April 2026 update

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// ./src/globals.d.ts
+declare module "*.css";
+declare module "@fontsource/*" {}
+declare module "@fontsource-variable/*" {}
